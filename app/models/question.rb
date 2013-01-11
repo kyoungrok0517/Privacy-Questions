9e84533e26a34fa23671094815ca51a6ca7c01fb
Kyoung-Rok Jang <astray0924@gmail.com>
2013-01-11 17:31:47 +0000

    dsfsdf

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,7 +1,7 @@
 class Question < ActiveRecord::Base
   attr_accessible :photo, :answer, :question_type
   
-  has_attached_file :photo, :styles => {:thumb => ["100x100#", :jpg], :large => ["400x400>", :jpg]},
+  has_attached_file :photo, :styles => {:thumb => ["100x100#", :jpg], :large => ["800x800>", :jpg]},
             :default_style => :thumb,
                     :url => "/:attachment/:id/:style/:basename.:extension",
                     :path => ":rails_root/public/:attachment/:id/:style/:basename.:extension"
